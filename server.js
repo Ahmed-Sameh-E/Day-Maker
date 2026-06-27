f582dc5d4fb3b798ee64ef2c85b48d57603f9b13
Ahmed Sameh <ahmedandmohammedelhelpawy@gmail.com>
2026-06-27 20:22:40 +0300

    Refactor server to use environment variable for port

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.post('/api/login', (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
